2dc4701e0d51645bfad652dd8e8e674fecfca857
carlosp420 <mycalesis@gmail.com>
2015-06-24 22:58:04 +0300

    mauno

---
+++ lunch_at_turku_univ/spiders/mauno.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import datetime
+
+import scrapy
+
+from lunch_at_turku_univ.items import LunchItem
+
+
+class MaunoSpider(scrapy.Spider):
+    name = "mauno"
+    allowed_domains = ["mauno.fi"]
+    start_urls = (
+        'http://www.mauno.fi/fi/keittio/lounasbuffet',
+    )
+
+    def parse(self, response):
+        today = datetime.date.today()
+        today_long_date = datetime.datetime.strftime(today, '%A, %d %b %Y')
+        today_in_numbers = datetime.datetime.strftime(today, '%d.%m').replace('.0', '.')
+
+        buffet = response.xpath("//p[contains(., '" + today_in_numbers + "')]/following-sibling::p[contains(., 'Lounasbuffet')]/text()").extract()
+
+        item = LunchItem()
+        item['restaurant'] = 'Mauno - Biocity'
+        item['dishes'] = [buffet[0]]
+        item['day'] = today_long_date
+
+        return item
