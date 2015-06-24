e6a498ddfbb84122952006e73cb8a348cd520cba
carlosp420 <mycalesis@gmail.com>
2015-06-24 22:32:13 +0300

    use processors in Item definition

--- lunch_at_turku_univ/items.py
+++ lunch_at_turku_univ/items.py
@@ -6,10 +6,14 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader.processors import MapCompose
+from w3lib.html import remove_tags
 
 
 class LunchItem(scrapy.Item):
     # define the fields for your item here like:
     day = scrapy.Field()
-    dishes = scrapy.Field()
+    dishes = scrapy.Field(
+        input_processor=MapCompose(remove_tags),
+    )
     restaurant = scrapy.Field()
